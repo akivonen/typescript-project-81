326eb9413965c7179bae9dc39162b7d55c3981c0
Artjom Kivonen <kivonen@gmail.com>
2024-10-02 20:32:54 +0300

    add Tag.tests

---
+++ src/__tests__/Tag.tests.ts
@@ -0,0 +1,21 @@
+import { expect, test } from 'vitest';
+import Tag from "../Tag";
+
+test('br', () => {
+  expect(new Tag("br").toString()).toBe('<br>');
+});
+test('input', () => {
+  expect(new Tag("input", { type: "submit", value: "Save" }).toString())
+    .toBe('<input type="submit" value="Save">');
+});
+test('label', () => {
+  expect(new Tag("label", {}, "Email").toString())
+    .toBe('<label>Email</label>');
+});
+test('label for', () => {
+  expect(new Tag("label", { for: "email" }, "Email").toString())
+    .toBe('<label for="email">Email</label>');
+});
+test('div', () => {
+  expect(new Tag("div").toString()).toBe('<div></div>');
+});
